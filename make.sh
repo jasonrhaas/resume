5272502d371e88b98cfe88ae84851330f143e09f
Jason Haas <jasonrhaas@gmail.com>
2018-05-21 16:18:54 -0500

    improve make file

--- make.sh
+++ make.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [ $# -eq 1 ]; then
-    name=${1//\.md/}
-    pandoc $1 -t plain -o $name.txt
-    pandoc $1 -t docx -o $name.docx
-    pandoc $1 -t html5 -o $name.html
+	name=$1
 else
-    echo "Usage : $0 YourResume.md"
-    exit 1
+	name="JasonHaas_Resume.md"
 fi
+new_name=${name//\.md/}
+pandoc $name -t plain -o $new_name.txt
+pandoc $name -t docx -o $new_name.docx
+pandoc $name -t html5 -o $new_name.html
 
