a2329ab9cba4e82148acf6c62b8815a5913006ff
Jason Haas <jasonrhaas@gmail.com>
2018-05-23 11:28:51 -0500

    only make pdf and txt files

--- make.sh
+++ make.sh
@@ -6,7 +6,8 @@ else
 	name="JasonHaas_Resume.md"
 fi
 new_name=${name//\.md/}
-pandoc $name -t plain -o $new_name.txt
-pandoc $name -t docx -o $new_name.docx
-pandoc $name -t html5 -o $new_name.html
 
+pandoc $name -t plain -o $new_name.txt
+# pandoc $name -t docx -o $new_name.docx
+# pandoc $name -t html5 -o $new_name.html
+pandoc $name -o $new_name.pdf
